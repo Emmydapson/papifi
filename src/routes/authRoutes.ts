88ef9258c4cbb8b04a9adfb05358b4d68c246ecb
emmydapson <iloriemmanuel00@gmail.com>
2024-10-20 23:29:05 +0100

    otp issue fixed

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,13 +2,13 @@
 import { Router } from 'express';
 import { registerUser, resendOtp, verifyOtp, loginUser, createTransactionPin } from '../controllers/authController';
 import KYCController from '../controllers/kycControllers';
-import { authMiddleware } from '../middlewares/authMiddleware';
+
 
 
 const router = Router();
 
 router.post('/register', registerUser);  // New route
-router.post('/verify-otp', authMiddleware, verifyOtp);
+router.post('/verify-otp',  verifyOtp);
 router.post('/login', loginUser);
 router.post('/resend-otp', resendOtp);
 console.log('Resend OTP route hit');
