de60a95f5f5d016e09c59df585aa0999b9fd48dc
emmydapson <iloriemmanuel00@gmail.com>
2024-11-20 10:46:34 +0100

    error fixed

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import {  getUserProfile, updateUserProfile } from '../controllers/profileController';
 import { authMiddleware } from '../middlewares/authMiddleware';
 import { changePassword } from '../controllers/profileController';
-import { submitSupportRequest } from '../controllers/profileController';
+
 
 const router = Router();
 
@@ -11,6 +11,6 @@ const router = Router();
 router.get('/', authMiddleware, getUserProfile);
 router.put('/', authMiddleware, updateUserProfile);
 router.put('/change-password', authMiddleware, changePassword);
-router.post('/support', authMiddleware, submitSupportRequest);
+
 
 export default router;
