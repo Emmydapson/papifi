546ea4778e25820bae5295683422fad5051f792b
emmydapson <iloriemmanuel00@gmail.com>
2025-11-20 13:21:57 +0100

    fixed

--- src/routes/kycRoutes.ts
+++ src/routes/kycRoutes.ts
@@ -4,8 +4,8 @@ import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.post('/kyc/start', authMiddleware, KYCController.startVerification);
-router.get('/kyc/status', authMiddleware, KYCController.getUserKYCStatus);
+router.post('/start', authMiddleware, KYCController.startVerification);
+router.get('/status', authMiddleware, KYCController.getUserKYCStatus);
 router.post('/', KYCController.webhook); // webhook from Dojah
 
 export default router;
