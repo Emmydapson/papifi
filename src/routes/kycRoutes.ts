ed96e45c30c94f3cec8b3092e1c7d27de9fb8bd9
emmydapson <iloriemmanuel00@gmail.com>
2025-12-14 12:24:25 +0100

    fixed

--- src/routes/kycRoutes.ts
+++ src/routes/kycRoutes.ts
@@ -1,19 +1,21 @@
-import { Router } from 'express';
+import express, { Router, Request, Response } from 'express';
 import KYCController from '../controllers/kycController';
 import { authMiddleware } from '../middlewares/authMiddleware';
+
 
 const router = Router();
 
 router.post('/start', authMiddleware, KYCController.startVerification);
 router.get('/status', authMiddleware, KYCController.getUserKYCStatus);
 router.post(
-  '/', 
+  '/',
   express.json({
-    verify: (req: any, res, buf) => {
+    verify: (req: Request & { rawBody?: string }, res: Response, buf: Buffer) => {
       req.rawBody = buf.toString();
-    }
+    },
   }),
   KYCController.webhook
 );
+
 
 export default router;
